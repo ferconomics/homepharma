8e86777d285371311ecb366f81c4026123448e32
Fer Moreno <fer@ferconomics.com>
2021-05-08 13:57:32 -0500

    first

---
+++ resources/views/template.blade.php
@@ -0,0 +1,23 @@
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Test</title>
+
+         
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+        
+    </head>
+
+    
+    <body>
+
+@include('_includes/nav/topnav)
+
+@yield('content')
+
+        
+</body>
+</html>
